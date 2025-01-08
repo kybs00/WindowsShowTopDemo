085127e70e0f53cabed08eeb51bb3473666dc5f6
冬 余 <1016724958@qq.com>
2025-01-08 18:09:20 +0800

    添加对锁屏/解锁场景的隐藏显示处理

--- WindowsShowTopDemo/MainWindow.xaml.cs
+++ WindowsShowTopDemo/MainWindow.xaml.cs
@@ -31,7 +31,9 @@ namespace WindowsShowTopDemo
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            SetWindowStyles(this);
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            _hWnd = helper.Handle;
+            SetTopmost();
         }
         const int GWL_EXSTYLE = -20;
         const int WS_EX_TOOLWINDOW = 0x00000080; // 不在任务栏显示
@@ -52,11 +54,12 @@ namespace WindowsShowTopDemo
         static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
         private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        private nint _hWnd;
 
-        public static void SetWindowStyles(Window window)
+        public void SetTopmost()
         {
-            WindowInteropHelper helper = new WindowInteropHelper(window);
-            IntPtr hWnd = helper.Handle;
+            IntPtr hWnd = _hWnd;
 
             int exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
             // 设置窗口样式为工具窗口, 不在任务栏显示
@@ -66,8 +69,23 @@ namespace WindowsShowTopDemo
             // 将窗口设置为顶层窗口
             SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
 
-            //任务栏不显示
-            window.ShowInTaskbar = false;
+            //二次设置任务栏不显示
+            ShowInTaskbar = false;
+        }
+        public void SetNoTopmost()
+        {
+            IntPtr hWnd = _hWnd;
+
+            int exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
+            // 设置窗口样式为工具窗口, 不在任务栏显示
+            exStyle |= WS_EX_TOOLWINDOW;
+            SetWindowLong(hWnd, GWL_EXSTYLE, exStyle);
+
+            // 将窗口设置为非顶层窗口
+            SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
+
+            //二次设置任务栏不显示
+            ShowInTaskbar = false;
         }
         private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
         {
@@ -75,11 +93,11 @@ namespace WindowsShowTopDemo
             {
                 //解锁屏
                 case SessionSwitchReason.SessionUnlock:
-                    Topmost = true;
+                    SetTopmost();
                     break;
                 //锁屏
                 case SessionSwitchReason.SessionLock:
-                    Topmost = false;
+                    SetNoTopmost();
                     break;
             }
         }

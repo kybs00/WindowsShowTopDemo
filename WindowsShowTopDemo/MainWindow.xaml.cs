426b7e620e0f944fb0d25d84331518d1fb2c3c09
冬 余 <1016724958@qq.com>
2025-01-08 17:28:12 +0800

    删除冗余代码

--- WindowsShowTopDemo/MainWindow.xaml.cs
+++ WindowsShowTopDemo/MainWindow.xaml.cs
@@ -21,25 +21,9 @@ namespace WindowsShowTopDemo
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Window _hiddenWindow;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            // 创建一个隐藏窗口
-            _hiddenWindow = new Window
-            {
-                Width = 0,
-                Height = 0,
-                ShowInTaskbar = false,
-                WindowStyle = WindowStyle.None // 确保不会显示在任务栏
-            };
-
-            _hiddenWindow.Show();
-
-            // 设置实际窗口的 Owner
-            this.Owner = _hiddenWindow;
             //当前登录的用户变化
             SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
             Loaded += MainWindow_Loaded;
@@ -51,7 +35,6 @@ namespace WindowsShowTopDemo
         }
         const int GWL_EXSTYLE = -20;
         const int WS_EX_TOOLWINDOW = 0x00000080; // 不在任务栏显示
-        const int WS_EX_TOPMOST = 0x00000008; // 置顶窗口
 
         const uint SWP_NOSIZE = 0x0001;
         const uint SWP_NOMOVE = 0x0002;
@@ -69,7 +52,6 @@ namespace WindowsShowTopDemo
         static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
         private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
-        private static readonly IntPtr HWND_TOP = new IntPtr(0);
 
         public static void SetWindowStyles(Window window)
         {
@@ -79,12 +61,12 @@ namespace WindowsShowTopDemo
             int exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
             // 设置窗口样式为工具窗口, 不在任务栏显示
             exStyle |= WS_EX_TOOLWINDOW;
-            exStyle &= ~WS_EX_TOPMOST; // 确保不是普通置顶
             SetWindowLong(hWnd, GWL_EXSTYLE, exStyle);
 
             // 将窗口设置为顶层窗口
             SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
 
+            //任务栏不显示
             window.ShowInTaskbar = false;
         }
         private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
@@ -100,13 +82,6 @@ namespace WindowsShowTopDemo
                     Topmost = false;
                     break;
             }
-        }
-
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            this.Owner = null;
-            _hiddenWindow?.Close();
-            this.Close();
         }
     }
 }
